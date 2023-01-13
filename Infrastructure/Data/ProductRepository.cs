7a10f43218fdc55b8a73ecf8cef8ee074a9bab69
reza <rezanaeimabadi7@gmail.com>
2023-01-13 10:31:29 -0700

    brought StoreContext to ProductRepository

--- Infrastructure/Data/ProductRepository.cs
+++ Infrastructure/Data/ProductRepository.cs
@@ -4,19 +4,27 @@ using System.Linq;
 using System.Threading.Tasks;
 using Core.Entities;
 using Core.Interfaces;
+using Microsoft.EntityFrameworkCore;
 
 namespace Infrastructure.Data
 {
     public class ProductRepository : IProductRepository
     {
-        public Task<Product> GetProductByIdAsync(int id)
+        public readonly StoreContext _context;
+        public ProductRepository(StoreContext context)
         {
-            throw new NotImplementedException();
+            _context = context;
+
         }
 
-        public Task<IReadOnlyList<Product>> GetProductsAsync()
+        public async Task<Product> GetProductByIdAsync(int id)
         {
-            throw new NotImplementedException();
+            return await _context.Products.FindAsync(id);
+        }
+
+        public async Task<IReadOnlyList<Product>> GetProductsAsync()
+        {
+            return await _context.Products.ToListAsync();
         }
     }
 }
