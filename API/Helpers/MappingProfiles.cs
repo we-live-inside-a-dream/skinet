fbfbb406b0d94f5ebf13b4dc9d25cdb5653cb2c7
Reza Naeim-Abadi <rezanaeimabadi7@gmail.com>
2023-05-16 07:30:52 -0600

    added configration to get productBrand and type

--- API/Helpers/MappingProfiles.cs
+++ API/Helpers/MappingProfiles.cs
@@ -10,7 +10,11 @@ namespace API.Helpers
     {
         public MappingProfiles()
         {
-            CreateMap<Product, ProductToReturnDto>();
+            CreateMap<Product, ProductToReturnDto>()
+            // "d" is the destination which lives in "ProductToReturnDto", "o" is for option which gets mapped and 
+            // "s" is for the source which is coming from ProductBand/ProductType...
+                .ForMember(d => d.ProductBrand, o => o.MapFrom(s => s.ProductBrand.Name))
+                .ForMember(d => d.ProductType, o => o.MapFrom(s => s.ProductType.Name));
         }
     }
 }
