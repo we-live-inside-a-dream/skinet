9c0627ccc67e18d2d8f2427008a11a019632ec24
Reza Naeim-Abadi <rezanaeimabadi7@gmail.com>
2023-05-28 11:33:11 -0600

    getting pictureUrl from the api Created ProductUrlResolver class and mapped the pictureUrl

--- API/Helpers/MappingProfiles.cs
+++ API/Helpers/MappingProfiles.cs
@@ -14,7 +14,8 @@ namespace API.Helpers
             // "d" is the destination which lives in "ProductToReturnDto", "o" is for option which gets mapped and 
             // "s" is for the source which is coming from ProductBand/ProductType...
                 .ForMember(d => d.ProductBrand, o => o.MapFrom(s => s.ProductBrand.Name))
-                .ForMember(d => d.ProductType, o => o.MapFrom(s => s.ProductType.Name));
+                .ForMember(d => d.ProductType, o => o.MapFrom(s => s.ProductType.Name))
+                .ForMember(d => d.PictureUrl, o => o.MapFrom<ProductUrlResolver>());
         }
     }
 }
