3bc1bfb6a4b189a0dacbd7de097c712ef6ddc9ea
Reza Naeim-Abadi <rezanaeimabadi7@gmail.com>
2023-06-04 02:43:50 -0600

    added ApiResponse to the controllers

--- API/Controllers/BuggyController.cs
+++ API/Controllers/BuggyController.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using API.Errors;
 using Infrastructure.Data;
 using Microsoft.AspNetCore.Mvc;
 
@@ -22,7 +23,7 @@ namespace API.Controllers
 
             if (thing == null)
             {
-                return NotFound();
+                return NotFound(new ApiResponse(404));
             }
             return Ok();
         }
@@ -40,7 +41,7 @@ namespace API.Controllers
         [HttpGet("badrequest")]
         public ActionResult GetBadRequest()
         {
-            return BadRequest();
+            return BadRequest(new ApiResponse(400));
         }
 
         [HttpGet("badrequest/{id}")]
